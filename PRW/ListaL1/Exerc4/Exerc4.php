25db33c8dc3b946d86b2049f2bbc15b71e9c4960
GuilhermeWeberMay <guilhermewebermay.dev@gmail.com>
2025-08-25 13:42:41 -0300

    Terminado o back-end

---
+++ PRW/ListaL1/Exerc4/Exerc4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title> Fundamentos do PHP </title>
+ <link rel="stylesheet" href="Exerc4.css">
+ <title> Fundamentos do PHP </title>
+</head>
+<body>
+ <h1> Fundamentos do PHP - Exerc4 - Respota servidor </h1>
+ <?php 
+ $fahrenheit = $_POST["fahrenheit"];
+ $celsius = ($fahrenheit - 32) * 5 / 9;
+ echo "<p> Resultado final: <br>
+       $celsius </p>
+ 
+ ";
+ ?>
+</body>
+</html>
