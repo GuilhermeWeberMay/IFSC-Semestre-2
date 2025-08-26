343a49c27abd6f4c61fd105a011ddbd8ea90e00f
GuilhermeWeberMay <guilhermewebermay.dev@gmail.com>
2025-08-26 13:04:28 -0300

    Update ExercAula01.sql

--- BDA/ExercAula01.sql
+++ BDA/ExercAula01.sql
@@ -18,9 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
 -- Banco de dados: `aula0108`
-
 -- --------------------------------------------------------
 -- Estrutura da tabela `pessoas`
 DROP TABLE IF EXISTS `pessoas`;
