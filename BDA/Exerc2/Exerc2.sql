2c8bdfc24b3b32fc2388b2e40e3fe964ea5aed11
GuilhermeWeberMay <guilhermewebermay.dev@gmail.com>
2025-08-26 15:05:33 -0300

    Update Exerc2.sql

---
+++ BDA/Exerc2/Exerc2.sql
@@ -0,0 +1,66 @@
+-- create database Exerc2;
+use Exerc2;
+
+set foreign_key_checks = 0;
+drop table if exists cursos;
+drop table if exists matriculas;
+drop table if exists disciplinas;
+drop table if exists alunos;
+set foreign_key_checks = 1;
+
+create table cursos (
+ id int primary key unique auto_increment not null,
+ nome varchar(256) not null,
+ descricao text not null
+ ) engine InnoDB;
+ 
+ create table disciplinas (
+ id int primary key unique auto_increment not null,
+ nome varchar(256) not null,
+ cursos_id int not null,
+ foreign key (cursos_id) references cursos(id)
+ ) engine InnoDB;
+ 
+ create table alunos (
+ id int primary key unique auto_increment not null,
+ nome varchar(512) not null,
+ email varchar(100) not null,
+ telefone varchar(20) not null
+ ) engine InnoDB;
+ 
+ create table matriculas (
+ id int primary key unique auto_increment not null,
+ alunos_id int not null,
+ cursos_id int not null,
+ disciplinas_id int not null,
+ foreign key (alunos_id) references alunos(id),
+ foreign key (cursos_id) references cursos(id),
+ foreign key (disciplinas_id) references disciplinas(id)
+ ) engine InnoDB;
+
+-- Povoamento do Banco de dados
+-- Cursos
+INSERT INTO cursos (nome, descricao) VALUES
+('Curso de Finanças', 'Aprenda os fundamentos das finanças no mercado luxemburguês.'),
+('Curso de Direito Internacional', 'Estudo das leis internacionais com foco na União Europeia.'),
+('Curso de Tecnologia da Informação', 'Introdução às tecnologias emergentes no setor de TI em Luxemburgo.'),
+('Curso de Gestão de Fundos', 'Conheça as melhores práticas para gestão de fundos de investimento.'),
+('Curso de Desenvolvimento Sustentável', 'Princípios e práticas para promover a sustentabilidade ambiental e econômica.');
+
+-- Disciplinas
+INSERT INTO disciplinas (nome, cursos_id) VALUES
+('Fundamentos de Economia', 1),
+('Legislação Europeia', 2),
+('Programação em Python', 3),
+('Análise de Investimentos', 4),
+('Gestão de Recursos Naturais', 5);
+
+-- Alunos
+INSERT INTO alunos (nome, email, telefone) VALUES
+('Lucas Müller', 'lucas.muller@example.lu', '+352 621 123 456'),
+('Sophie Weber', 'sophie.weber@example.lu', '+352 621 654 321'),
+('Maximilian Schmit', 'max.schmit@example.lu', '+352 621 789 012'),
+('Emma Hoffmann', 'emma.hoffmann@example.lu', '+352 621 345 678'),
+('Marie Dupont', 'marie.dupont@example.lu', '+352 621 901 234');
+
+select * from matriculas
