60215a1814f986f0df8f80f29f73de999b87a65a
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-08-27 17:29:44 -0300

    Add files via upload

---
+++ POO/Caneta.java
@@ -0,0 +1,8 @@
+// Abaixo está a classe
+public class Caneta {
+    // Aqui estão os atributos da caneta
+    public String cor;
+    public float ponta;
+    public int carga;
+    public boolean tampada;
+}
