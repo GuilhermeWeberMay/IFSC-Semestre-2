ce3d68da6135e71ff03ae2733a251d00deaf12bc
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-01 14:36:58 -0300

    Criado os arquivos

---
+++ PRW/ListaL2/Exerc1/Exerc1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width,initial-scale=1.0">
+ <title> Vetores em PHP </title>
+</head>
+<body>
+ <?php
+ // Misturar os tipos de dados NÃO é uma boa prática, por mais que a linguagem permita 
+ ?>
+</body>
+</html>
