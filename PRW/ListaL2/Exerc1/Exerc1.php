b6c0000f8e8dde59b3267cc121f83b5562839dfa
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-01 16:04:48 -0300

    Exerc1 finalizado

--- PRW/ListaL2/Exerc1/Exerc1.php
+++ PRW/ListaL2/Exerc1/Exerc1.php
@@ -2,12 +2,32 @@
 <html lang="pt-BR">
 <head>
  <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width,initial-scale=1.0">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="Exerc1.css">
  <title> Vetores em PHP </title>
 </head>
 <body>
+ <h1> Vetores em PHP - ListaL2 - Exerc1 </h1>
  <?php
- // Misturar os tipos de dados NÃO é uma boa prática, por mais que a linguagem permita 
+
+ $nota1 = $_POST['nota1'];
+ $nota2 = $_POST['nota2'];
+ $nota3 = $_POST['nota3'];
+
+ // Criando um vetor em PHP a partir das três variáveis escalares
+ $vetorNotas = [$nota1, $nota2, $nota3];
+ // Misturar os tipos de dados NÃO é uma boa prática, por mais que a linguagem permita
+
+ // Somando as três notas do vetor
+ $soma = array_sum($vetorNotas);
+ // O PHP somente irá usar o array_sum quando os todas as casas forem números 
+
+ // Calculando a média de notas do vetor
+ $media = $soma / count($vetorNotas);
+ // COUNT é responsavel por contar quantos indices tem o vetor 
+
+ $mediaFormatada = number_format($media, 1, ",", ".");
+ echo "<p> A média de nota dos 3 alunos = <span> $mediaFormatada </span> </p>"
  ?>
 </body>
 </html>
