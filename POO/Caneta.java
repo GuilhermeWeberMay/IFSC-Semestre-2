f9b6b3076f30c8ebc76e850592c67b906ac19afb
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-03 09:01:10 -0300

    Material POOI

--- POO/Caneta.java
+++
@@ -1,8 +0,0 @@
-// Abaixo está a classe
-public class Caneta {
-    // Aqui estão os atributos da caneta
-    public String cor;
-    public float ponta;
-    public int carga;
-    public boolean tampada;
-}
