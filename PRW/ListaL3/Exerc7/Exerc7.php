b8f1a67f87604f7198b8d3c92a39ba1c3499865d
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-18 20:22:53 -0300

    Começo css

--- PRW/ListaL3/Exerc7/Exerc7.php
+++ PRW/ListaL3/Exerc7/Exerc7.php
@@ -4,6 +4,7 @@
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title> Matrizes PHP </title>
+  <link rel="stylesheet" href="Exerc7.css">
 </head>
 <body>
  <h1> Matrizes em PHP - ListaL3 - Exerc7 </h1>
