a259ac100b0c41a1163976ba4ba915b88a164b7b
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-10-13 14:21:17 -0300

    Update Exerc5.php

--- PRW/ListaL4/Exerc5/Exerc5.php
+++ PRW/ListaL4/Exerc5/Exerc5.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Cálculo da Média do Aluno</title>
+ <title>Funções de usuário na linguagem PHP - subprogramação </title>
  <link rel="stylesheet" href="Exerc5.css">
 </head>
 
 <body>
- <form action="Exerc5.php" method="post">
-  <h2>Calcular Média</h2>
+ <h1> Funções de usuário em PHP - Exercício 5 </h1>
 
+ <form action="Exerc5.php" method="post">
+  
   <label for="nota1">Nota 1:</label>
-  <input type="number" id="nota1" name="nota1" step="0.1" required min="0" max="10" > <br>
+  <input type="number" id="nota1" name="nota1" step="0.1" required min="0" max="10"> <br>
 
   <label for="nota2">Nota 2:</label>
-  <input type="number" id="nota2" name="nota2" step="0.1" required min="0" max="10" > <br>
+  <input type="number" id="nota2" name="nota2" step="0.1" required min="0" max="10"> <br>
 
   <button name="calcular"> Calcular média </button>
  </form>
  <?php
 
- if(isset($_POST['calcular'])){
- 
- include "calculaMedia.inc.php";
- include "validaSituacao.inc.php";
+ if (isset($_POST['calcular'])) {
 
- $nota1 = $_POST['nota1'];
- $nota2 = $_POST['nota2'];
+  include "calculaMedia.inc.php";
+  include "validaSituacao.inc.php";
 
- $media =  calcularMedia($nota1, $nota2);
- $situacao = validaSituacao($media);
- $media = number_format($media, 1, ",", ".");
- echo "<p class=resultado>$situacao e $media</p>";
+  $nota1 = $_POST['nota1'];
+  $nota2 = $_POST['nota2'];
 
+  $media =  calcularMedia($nota1, $nota2);
+  $situacao = validaSituacao($media);
+  $media = number_format($media, 1, ",", ".");
+  echo "<p class=resultado>$situacao e $media</p>";
  }
  ?>
 </body>
